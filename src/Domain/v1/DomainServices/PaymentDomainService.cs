966edf5ef54bc13abc53cd6310721d7b6a75506d
Alexandre Betassa de Souza <5by5.betassa@voeazul.com.br>
2025-04-27 12:05:54 -0300

    add try catch no metodo DebitCouponCodeAsync

--- src/Domain/v1/DomainServices/PaymentDomainService.cs
+++ src/Domain/v1/DomainServices/PaymentDomainService.cs
@@ -1,11 +1,13 @@
 ﻿using Fatec.Store.Orders.Domain.v1.Interfaces.ServiceClients;
 using Fatec.Store.Orders.Domain.v1.Interfaces.Services;
+using Microsoft.Extensions.Logging;
 
 namespace Fatec.Store.Orders.Domain.v1.DomainServices
 {
-    public class PaymentDomainService(ICouponServiceClient couponServiceClient) : IPaymentDomainService
+    public class PaymentDomainService(ICouponServiceClient couponServiceClient, ILoggerFactory loggerFactory) : IPaymentDomainService
     {
         private readonly ICouponServiceClient _couponServiceClient = couponServiceClient;
+        private readonly ILogger<PaymentDomainService> _logger = loggerFactory.CreateLogger<PaymentDomainService>();
 
         public async Task<decimal> CalculateDiscountAsync(decimal originalAmount, string couponCode)
         {
@@ -17,7 +19,16 @@ namespace Fatec.Store.Orders.Domain.v1.DomainServices
             return decimal.Zero;
         }
 
-        public async Task DebitCouponCodeAsync(string couponCode, int userId) =>
-            await _couponServiceClient.DebitCouponCodeAsync(new() { CouponCode = couponCode, UserId = userId });
+        public async Task DebitCouponCodeAsync(string couponCode, int userId)
+        {
+            try
+            {
+                await _couponServiceClient.DebitCouponCodeAsync(new() { CouponCode = couponCode, UserId = userId });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex, "{Service}.{Method}", nameof(PaymentDomainService), nameof(DebitCouponCodeAsync));
+            }
+        }
     }
 }
